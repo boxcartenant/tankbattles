b4b35e8b997cadaf47d7c5743055dcbc268291c3
boxcartenant <zactheslade@gmail.com>
2024-06-18 16:32:56 -0400

    Update netcode.py
    A lot of changes trying to compensate for lost messages during testing: different kinds of threading, queuing, locks, TCP, buffer sizes, etc... The last change that actually stopped all the dropped messages I was seeing was the addition of time.sleep(0.01) in send_messages(). Removing some of those other features might improve performance, idk.

--- netcode.py
+++ netcode.py
@@ -1,10 +1,11 @@
 import socket
 import threading
+import queue
 import time
 import ast
-
-
-WAIT_TIMEOUT = 1000 #timeout to wait for payload after receiving header.
+#import pickle
+import dill
+from bfield_unit import net_unit_archetype
 
 #Usage:
 #
@@ -23,6 +24,7 @@ WAIT_TIMEOUT = 1000 #timeout to wait for payload after receiving header.
 # myclient.stop()
 
 class ServerClient:
+    #this class is intended to manage exactly one server XOR one client, not both.
     def __init__(self, host='localhost', port=12345, callback_dict = {}):
         self.host = host
         self.port = port
@@ -31,10 +33,23 @@ class ServerClient:
         self.lock = threading.Lock()
         self.running = threading.Event()
         self.running.set()
+        self.outbound_queue = queue.Queue()
+        self.inbound_queue = queue.Queue()
+        self.isServer = False
+        self.isClient = False
+        self.message_id = 0
+        #self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=4)
+
+    def initialize(self, host='localhost', port=12345, callback_dict = {}):
+        self.host = host
+        self.port = port
+        self.callback_dict = callback_dict
 
     def stop(self):
         # Turn off the server or client
         self.running.clear()
+        self.receive_thread.join()
+        self.send_thread.join()
         if self.net_socket:
             self.net_socket.shutdown(socket.SHUT_RDWR)
             self.net_socket.close()
@@ -46,48 +61,86 @@ class ServerClient:
         server_socket.bind((self.host, self.port))
         server_socket.listen(1)
         print("server is listening on port", self.port)
-
         self.net_socket, addr = server_socket.accept()
         print(f"connection from {addr} has been established.")
-
-        threading.Thread(target=self.handle_incoming, args=(self.net_socket,)).start()
+        self.isServer = True
+        self.receive_thread = threading.Thread(target=self.handle_incoming, args=(self.net_socket,))
+        self.receive_thread.start()
+        self.processing_thread = threading.Thread(target=self.process_messages)
+        self.processing_thread.start()
+        self.send_thread = threading.Thread(target=self.send_messages)
+        self.send_thread.start()
     
     def start_client(self):
         self.net_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.net_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 1024*1024) #trying to overcome some message loss issues
         self.net_socket.connect((self.host, self.port))
         print(f"Connected to server at {self.host}:{self.port}")
-        threading.Thread(target=self.handle_incoming, args=(self.net_socket,)).start()
+        self.isClient = True
+        self.receive_thread = threading.Thread(target=self.handle_incoming, args=(self.net_socket,))
+        self.receive_thread.start()
+        self.processing_thread = threading.Thread(target=self.process_messages)
+        self.processing_thread.start()
+        self.send_thread = threading.Thread(target=self.send_messages)
+        self.send_thread.start()
 
 
     def send(self, message, payload):
         #locks and sends two separate messages
         with self.lock:
-            self.net_socket.send(message.encode())
-            self.net_socket.send(str(payload).encode())
+            self.outbound_queue.put([message,payload])
+
+    def send_messages(self):
+        while self.running.is_set():
+            message = self.outbound_queue.get()
+            with self.lock:
+                #self.net_socket.sendall(pickle.dumps(message))
+                self.net_socket.sendall(dill.dumps(message))
+            time.sleep(0.01)
 
     def handle_incoming(self,*arrrrrgs):
         msg = None
-        payload = None
+        self.net_socket.settimeout(3)
         while self.running.is_set():
-            msg = self.net_socket.recv(1024).decode()
-            if msg:
-                print("received:",msg, type(msg))
-            if str(msg) in self.callback_dict.keys():
-                start_time = time.time()
-                payload = self.net_socket.recv(1024).decode()
-                if payload:
-                    print("payload:",payload)
-                    try:
-                        payload_data = ast.literal_eval(payload)
-                    except (ValueError, SyntaxError):
-                        payload_data = payload
-                    self.callback_dict[msg](payload_data)
-            msg = None
-            payload = None
+            try:
+                #msg = pickle.loads(self.net_socket.recv(2048))
+                msg = self.net_socket.recv(2048)
+                with self.lock:
+                    #there was an issue with message loss, so we're trying to process incoming as fast as possible.
+                    self.inbound_queue.put(msg)
+            except TimeoutError as e:
+                pass
+            #finally:
+                #self.net_socket.settimeout(None)
+            
+            
 
+    def process_messages(self):
+        msg = None
+        while self.running.is_set():
+            try:
+                msg = dill.loads(self.inbound_queue.get())
+                if msg:
+                    if str(msg[0]) in self.callback_dict.keys() and (len(msg) > 1):
+                        #start_time = time.time()
+                        callback_thread = threading.Thread(target=self.callback_dict[msg[0]], args=(msg[1],), daemon=True)
+                        callback_thread.start()
+                        #self.callback_dict[msg[0]](msg[1])
+                msg = None
+            except Exception as e:
+                print("process messages error:",e)
+
+
+#### for testing...
+            
 #def showpayloads(payload):
-#    print(payload)
-#
+#    print(payload, type(payload))
 #testdict = {"a":showpayloads}
+
 #myServer = ServerClient(host='localhost', port=12345, callback_dict = testdict)
+#myServer.start_server()
+
 #myClient = ServerClient(host='localhost', port=12345, callback_dict = testdict)
+#myClient.start_client()
+#myunit = net_unit_archetype(x=1, y=2, team_color="red")
+#myClient.send("a",myunit)
