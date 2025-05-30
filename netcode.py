56fb36e6dcaf22b4074a9117bf67e34c9b4ea36b
boxcartenant <zactheslade@gmail.com>
2025-05-30 19:40:49 -0400

    Update netcode.py
    Ch ch ch changes

--- netcode.py
+++ netcode.py
@@ -5,6 +5,7 @@ import time
 import ast
 #import pickle
 import dill
+import struct
 from bfield_unit import net_unit_archetype
 from tkinter import messagebox
 
@@ -87,6 +88,7 @@ class ServerClient:
                 break
         if connected:
             self.isServer = True
+            self.net_socket.settimeout(10)  # Set timeout for socket operations
             self.receive_thread = threading.Thread(target=self.handle_incoming, args=(self.net_socket,))
             self.receive_thread.start()
             self.processing_thread = threading.Thread(target=self.process_messages)
@@ -101,6 +103,7 @@ class ServerClient:
         self.net_socket.connect((self.host, self.port))
         print(f"Connected to server at {self.host}:{self.port}")
         self.isClient = True
+        self.net_socket.settimeout(10)  # Set timeout for socket operations
         self.receive_thread = threading.Thread(target=self.handle_incoming, args=(self.net_socket,))
         self.receive_thread.start()
         self.processing_thread = threading.Thread(target=self.process_messages)
@@ -112,30 +115,43 @@ class ServerClient:
     def send(self, message, payload):
         #locks and sends two separate messages
         with self.lock:
-            self.outbound_queue.put([message,payload])
+            self.outbound_queue.put([message, payload])
 
     def send_messages(self):
         while self.running.is_set():
             message = self.outbound_queue.get()
             with self.lock:
-                #self.net_socket.sendall(pickle.dumps(message))
-                self.net_socket.sendall(dill.dumps(message))
-            time.sleep(0.01)
+                message_bytes = dill.dumps(message)
+                length = len(message_bytes)
+                self.net_socket.sendall(struct.pack('!I', length))  # Send length as 4-byte unsigned int
+                self.net_socket.sendall(message_bytes)
+            time.sleep(0.01)  # Consider adjusting or removing based on performance
 
-    def handle_incoming(self,*arrrrrgs):
+    def recvall(self, sock, n):
+        data = b''
+        while len(data) < n:
+            packet = sock.recv(n - len(data))
+            if not packet:
+                return None
+            data += packet
+        return data
+
+    def handle_incoming(self, *arrrrrgs):
         msg = None
         self.net_socket.settimeout(3)
         while self.running.is_set():
             try:
-                #msg = pickle.loads(self.net_socket.recv(2048))
-                msg = self.net_socket.recv(2048)
+                length_bytes = self.recvall(self.net_socket, 4)
+                if not length_bytes:
+                    break
+                length = struct.unpack('!I', length_bytes)[0]
+                message_bytes = self.recvall(self.net_socket, length)
+                if not message_bytes:
+                    break
                 with self.lock:
-                    #there was an issue with message loss, so we're trying to process incoming as fast as possible.
-                    self.inbound_queue.put(msg)
-            except TimeoutError as e:
-                pass
-            #finally:
-                #self.net_socket.settimeout(None)
+                    self.inbound_queue.put(message_bytes)
+            except Exception as e:
+                print("Error in handle_incoming:", e)
             
             
 
@@ -143,16 +159,16 @@ class ServerClient:
         msg = None
         while self.running.is_set():
             try:
-                msg = dill.loads(self.inbound_queue.get())
-                if msg:
-                    if str(msg[0]) in self.callback_dict.keys() and (len(msg) > 1):
+                msg_bytes = self.inbound_queue.get()
+                msg = dill.loads(msg_bytes)
+                if msg and str(msg[0]) in self.callback_dict and len(msg) > 1:
                         #start_time = time.time()
-                        callback_thread = threading.Thread(target=self.callback_dict[msg[0]], args=(msg[1],), daemon=True)
-                        callback_thread.start()
+                    callback_thread = threading.Thread(target=self.callback_dict[msg[0]], args=(msg[1],), daemon=True)
+                    callback_thread.start()
                         #self.callback_dict[msg[0]](msg[1])
                 msg = None
             except Exception as e:
-                print("process messages error:",e)
+                print("process messages error:", e)
 
 
 #### for testing...
